1cb75f2297bc5289763e435e97cff227cd6e18f2
Ben Anderson <benandrs@gmail.com>
2022-07-08 16:54:20 +1000

    Add very thin Rust interface

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,13 +1,14 @@
 
-use termbox2::*;
+use termbox2::{Term, Style, Color};
 
 fn main() {
-    let mut ev = Event::default();
-    unsafe {
-        tb_init();
-        tb_clear();
-        tb_present();
-        tb_poll_event(&mut ev);
-        tb_shutdown();
-    }
+    let term = Term::new().unwrap();
+    term.clear();
+    term.set_cell(0, 0, 'H', Style::None, Color::Default, Color::Default);
+    term.set_cell(1, 0, 'e', Style::None, Color::Default, Color::Default);
+    term.set_cell(2, 0, 'l', Style::None, Color::Default, Color::Default);
+    term.set_cell(3, 0, 'l', Style::None, Color::Default, Color::Default);
+    term.set_cell(4, 0, 'o', Style::None, Color::Default, Color::Default);
+    term.present();
+    term.poll_event();
 }
